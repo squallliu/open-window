9f2db426ce88a771a832254b87c1d9cd1d1c78e1
squall <squall.liu@gmail.com>
2016-07-28 08:24:04 +0800

    加沉浸状态栏支持

--- whcyit-open-window.js
+++ whcyit-open-window.js
@@ -1,60 +1,64 @@
-var whcyitModule = angular.module('whcyit-open-window', ['ionic']);
+angular.module('whcyit-open-window', ['ionic']);
 
-whcyitModule.factory('cyOpenWindow', ['$rootScope', '$ionicModal', '$sce', function ($rootScope, $ionicModal, $sce) {
-  var modalTemplate =
-    '<ion-modal-view>' +
-      '<ion-header-bar align-title="center" class="bar bar-{{::theme}}" ng-if="title">' +
-        '<h1 class="title" ng-bind-html="::title"></h1>' +
-        '<button class="button button-clear button-icon {{::close}}" ng-click="closeModal()"></button>' +
-      '</ion-header-bar>' +
-      '<ion-content scroll="true" class="no-padding open-window" ng-class="{\'has-header\': title}">' +
-        '<iframe data-tap-disabled="true" ng-src="{{url}}"></iframe>' +
-      '</ion-content>' +
-    '</ion-modal-view>';
+(function (angular, document) {
+  angular.module('whcyit-open-window', ['ionic']).factory('cyOpenWindow', ['$rootScope', '$ionicModal', '$sce', function ($rootScope, $ionicModal, $sce) {
+    var modalTemplate =
+      '<ion-modal-view>' +
+        '<cy-immerse-bar ng-if="isImmerse" bar-class="bar-{{::theme}}"></cy-immerse-bar>' +
+        '<ion-header-bar align-title="center" class="bar bar-{{::theme}}" ng-if="title">' +
+          '<h1 class="title" ng-bind-html="::title"></h1>' +
+          '<button class="button button-clear button-icon {{::close}}" ng-click="closeModal()"></button>' +
+        '</ion-header-bar>' +
+        '<ion-content scroll="true" class="no-padding open-window" ng-class="{\'has-header\': title}">' +
+          '<iframe data-tap-disabled="true" ng-src="{{url}}"></iframe>' +
+        '</ion-content>' +
+      '</ion-modal-view>';
 
-  return {
-    show: function (opts) {
-      var scope = $rootScope.$new(true);
-      angular.extend(scope, {
-        url: 'about:blank',
-        theme: 'positive',
-        title: '',
-        close: ionic.Platform.isAndroid() ? 'ion-android-close' : 'ion-ios-close',
-        onmessage: angular.noop
-      }, opts);
+    return {
+      show: function (opts) {
+        var scope = $rootScope.$new(true);
+        angular.extend(scope, {
+          url: 'about:blank',
+          theme: 'positive',
+          title: '',
+          isImmerse: document.body.querySelector('.status-bar') ? true : false,
+          close: ionic.Platform.isAndroid() ? 'ion-android-close' : 'ion-ios-close',
+          onmessage: angular.noop
+        }, opts);
 
-      if (scope.url) {
-        scope.url = $sce.trustAsResourceUrl(scope.url);
-      }
+        if (scope.url) {
+          scope.url = $sce.trustAsResourceUrl(scope.url);
+        }
 
-      window.addEventListener('message', function (e) {
-        scope.onmessage(e);
-      }, false);
-
-      var hide = function () {
-        scope.url = $sce.trustAsResourceUrl('about:blank');
-        window.removeEventListener('message', function () {
+        window.addEventListener('message', function (e) {
+          scope.onmessage(e);
         }, false);
-        scope.modal.remove();
-        scope.modal = null;
-      };
 
-      scope.showModal = function () {
-        scope.modal = $ionicModal.fromTemplate(modalTemplate, {
-          scope: scope
-        });
-        scope.modal.show();
-      };
+        var hide = function () {
+          scope.url = $sce.trustAsResourceUrl('about:blank');
+          window.removeEventListener('message', function () {
+          }, false);
+          scope.modal.remove();
+          scope.modal = null;
+        };
 
-      scope.closeModal = function () {
-        hide();
-      };
+        scope.showModal = function () {
+          scope.modal = $ionicModal.fromTemplate(modalTemplate, {
+            scope: scope
+          });
+          scope.modal.show();
+        };
 
-      scope.showModal();
-      
-      return {
-        hide: hide
-      };
-    }
-  };
-}]);
+        scope.closeModal = function () {
+          hide();
+        };
+
+        scope.showModal();
+
+        return {
+          hide: hide
+        };
+      }
+    };
+  }]);
+})(angular, document);
