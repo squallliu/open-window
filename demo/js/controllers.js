942030874b0884755d8c890ef9bded78eb3c6a44
squall <squall.liu@gmail.com>
2016-07-29 08:20:15 +0800

    删除demo，见https://github.com/squallliu/all-in-one-demo

--- demo/js/controllers.js
+++
@@ -1,9 +0,0 @@
-angular.module('starter.controllers', []).controller('HomeCtrl', function ($scope, cyOpenWindow) {
-  $scope.open = function () {
-    var window = cyOpenWindow.show({
-      url: 'http://www.baidu.com',
-      title: '百度'
-    }); 
-    // window.hide();
-  };
-});
